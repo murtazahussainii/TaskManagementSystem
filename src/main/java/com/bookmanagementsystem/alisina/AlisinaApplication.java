54341b5d09235b42ced00eece68819f85563fa75
Ali Sina <ali.sina1447@gmail.com>
2024-10-15 01:11:54 -0400

    spring initialized

---
+++ src/main/java/com/bookmanagementsystem/alisina/AlisinaApplication.java
@@ -0,0 +1,13 @@
+package com.bookmanagementsystem.alisina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlisinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlisinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmanagementsystem/alisina/AlisinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmanagementsystem.alisina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlisinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
